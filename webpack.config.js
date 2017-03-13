91503cb15be8a59b542447b63cd67dee4e4e9214
Weili.Cai <caiwlwinnie@gmail.com>
2017-03-13 10:50:56 +0800

    webpack生成的带目录结构 添加gulp的配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,40 @@
  * @Created Date:2017/3/2
  */
 var path = require('path');
+var glob = require('glob');
 var webpack = require('webpack');
+var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+var UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+var ProvidePlugin = require("webpack/lib/ProvidePlugin");
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+/**
+ * webpack 入口文件键值堆
+ * @param root 项目工程路径
+ * @returns {{}}
+ */
+function getExports (root) {
+    var files = glob.sync(root);
+    var entries = {}, entry, dirname, basename;
+
+    files.forEach(function (file) {
+        entry = file;
+        dirname = path.dirname(entry);
+        basename = path.basename(entry, '.js');
+        entries[path.join(dirname, basename)] = entry;
+    });
+
+    return entries;
+};
 
 module.exports = {
-    entry: {
-        'main': path.resolve(__dirname, 'app/app.js'),
-        'app2': path.resolve(__dirname, 'app/app2.js'),
-    },
+    entry: Object.assign({}, getExports("./app/**/*.js"), {common: ['jquery', 'bootstrap']}),
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: "bundle.js"
+        filename: "[name].js"
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
@@ -32,16 +53,44 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: 'style!css'
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {modules: true}
+                    }
+                ]
+            }, {
+                test: /\.less$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "less-loader"
+                ]
             }
         ]
     },
-    plugins:[
+    plugins: [
 
-        // 拆分插件
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'app2',
-            filename: 'bundle2.js' //这个生成的是公共文件，必须先引入
+        //将公共代码抽离出来合并为一个文件
+        new CommonsChunkPlugin({
+            name: "common",
+            filename: "common.js",
+            minChunks: 5
+        }),
+        //提供全局的变量，在模块中使用无需用require引入
+        new ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+
+        //js文件的压缩
+        new UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
         })
     ]
 };
